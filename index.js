0d923d06b3c80a0902c3f26b72e309fd0502860e
Paul Donovan <paul.m.donovan@outlook.com>
2024-03-10 10:54:27 +1100

    CommitSolution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records) {
+    // use the find metho to look for a winning record
+
+    const winningRecord = records.find(record => record.result === 'W');
+    return winningRecord ? winningRecord.year : undefined;
+}
+const winningYear = superbowlWin(record);
+console.log(winningYear);
